8d015ffbec1ee778a70cfdb89b2086733dcacd26
David Rivas <jdr@rivas-yee.com>
2016-11-23 19:20:38 -0800

    Added TaskDefinition to the resouce map to make longs arns.

--- arn.go
+++ arn.go
@@ -20,10 +20,29 @@ func ShortArnString(arn *string) (s string) {
   return shortArn
 }
 
+
+// To add a new type, add the const below and then the definition in the arnResourceMap
 type ResourceType int
 const (
   ContainerInstanceType ResourceType = iota
+  TaskDefinitionType
 )
+
+// The general ARN form is  "arn:aws:<service>:<region>:<account-number>:<resource-type>/<shortArn>"
+// Example ARN: arn:aws:ecs:us-east-1:033441544097:task-definition/craft-logstash:4
+// In this exapmle the typeString is the string "task-definiiton"
+// the servicePrefix is constructed from the <service> and the session.Session passed, in this case "ecs'"
+// Of course this will be very brittle and require a lot of following along if AWS makes changes.
+// TODO: make this easier to fill out ideally: var arnResource [][]{ {TaskDefinitionType, "task-definition", "ecs"} }
+// typedef resourceEntry struct {ResourceType, Type: string, Service: string}
+// var arnEntryList{} []resourceEntry{
+//  {ResourceType: TaskDefinitiontype, Type: "task-definition", Service: "ecs",},
+//}
+// Then fill out the arnResourceMap at init time.
+var arnResourceMap = map[ResourceType]resourceArnValues{
+  ContainerInstanceType: { typeString: "container-instance", servicePrefix: makeServicePrefix("ecs") },
+  TaskDefinitionType: { typeString: "task-definition", servicePrefix: makeServicePrefix("ecs") },
+}
 
 type resourceArnValues struct {
   typeString string
@@ -32,9 +51,6 @@ type resourceArnValues struct {
 
 func makeServicePrefix(service string) (string) {
   return fmt.Sprintf("arn:aws:%s:", service)
-}
-var arnResourceMap = map[ResourceType]resourceArnValues{
-  ContainerInstanceType: {typeString: "container-instance", servicePrefix: makeServicePrefix("ecs")},
 }
 
 func makeLong(prefix, region, an, resource, shortArn string) (string) {
